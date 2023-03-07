62e382bd1b649a7b514abbab2140523d3aa06d29
Kharol Chicaiza <kharol.chicaiza@epn.edu.ec>
2023-03-07 13:21:11 -0500

    fr-login y logout
    Administradores y clientes

--- tiendaChocolates/src/app/components/login-administradores/login-administradores.component.ts
+++ tiendaChocolates/src/app/components/login-administradores/login-administradores.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Administrador } from 'src/app/models/administrador';
+import { AdministradorService } from 'src/app/services/administrador.service';
+import { Global } from 'src/app/services/global';
 
 @Component({
   selector: 'app-login-administradores',
   templateUrl: './login-administradores.component.html',
-  styleUrls: ['./login-administradores.component.css']
+  styleUrls: ['./login-administradores.component.css'],
+  providers:[AdministradorService]
 })
-export class LoginAdministradoresComponent {
+export class LoginAdministradoresComponent implements OnInit{
+  public url:string;
+  public usuario:string;
+  public contrasenia:string;
+  public idGuardado:string;
+  public admin:Administrador;
 
+  constructor(
+    private _adminService:AdministradorService,
+    private _router:Router,
+
+  ){
+    this.url=Global.url;
+    this.usuario="";
+    this.idGuardado="";
+    this.contrasenia="";
+    this.admin=new Administrador('','','','','','');
+  }
+  ngOnInit(): void {
+    
+  }
+
+  loginAdmin(form: NgForm) {
+    this._adminService.login(this.usuario, this.contrasenia).subscribe(
+      response => {
+        this.admin=response.administrador;
+        this._router.navigate(['/home',this.admin._id]);
+
+      },
+      error => {
+        console.log(error);
+
+      }
+    );
+  }
 }
--- tiendaChocolates/src/app/components/login-clientes/login-clientes.component.ts
+++ tiendaChocolates/src/app/components/login-clientes/login-clientes.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Cliente } from 'src/app/models/cliente';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { Global } from 'src/app/services/global';
 
 @Component({
   selector: 'app-login-clientes',
   templateUrl: './login-clientes.component.html',
-  styleUrls: ['./login-clientes.component.css']
+  styleUrls: ['./login-clientes.component.css'],
+  providers:[ClienteService]
 })
-export class LoginClientesComponent {
+export class LoginClientesComponent implements OnInit{
+  public url:string;
+  public usuario:string;
+  public contrasenia:string;
+  public idGuardado:string;
+  public cliente:Cliente;
 
+  constructor(
+    private _clienteService:ClienteService,
+    private _router:Router,
+
+  ){
+    this.url=Global.url;
+    this.usuario="";
+    this.idGuardado="";
+    this.contrasenia="";
+    this.cliente=new Cliente('','','',false,'','','');
+  }
+  ngOnInit(): void {
+    
+  }
+
+  loginCliente(form: NgForm) {
+    this._clienteService.login(this.usuario, this.contrasenia).subscribe(
+      response => {
+        this.cliente=response.cliente;
+        this._router.navigate(['/home',this.cliente._id]);
+
+      },
+      error => {
+        console.log(error);
+
+      }
+    );
+  }
 }
